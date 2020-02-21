40e327f6532b37251b61d7bdd4636c0fee0e4db3
andrea <info@shlema.info>
2020-02-21 16:16:25 +0200

    validait

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core'
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  form: FormGroup
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl('', [
+          Validators.email,
+          Validators.required]),
+      password: new FormControl(null, [
+          Validators.required,
+          Validators.minLength(6)
+      ])
+
+    })
+  }
+
+  submit() {
+    if (this.form.valid) {
+      console.log('Form submitted: ', this.form);
+      const formDate = {...this.form.value};
+      console.log('Form data', formDate);
+    }
+  }
+}
+
